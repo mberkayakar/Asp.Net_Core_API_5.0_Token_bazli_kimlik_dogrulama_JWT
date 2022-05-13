617c0d3ed6b550538d66592229eb793c4cc0ea12
mberkayakar <m.berkay.akar@gmail.com>
2022-05-13 13:48:18 +0300

    added generic repository in service layer

---
+++ AKAR_JWT_PROJECT/AKAR.AuthService.Services/DtoMapper.cs
@@ -0,0 +1,21 @@
+﻿using AKAR.AuthServer.Core.DTOs;
+using AKAR.AuthServer.EntityLayer;
+using AKAR.AuthServer.EntityLayer.Concrete;
+using AutoMapper;
+using System;
+
+namespace AKAR.AuthService.Services
+{
+    public class DtoMapper:Profile
+    {
+        public DtoMapper()
+        {
+            // 10.00 
+            // reverse map product tan dto ya dto dan ise prdouct a dönebilir manasına gelmektedir.    
+            CreateMap<ProductDTO,Product>().ReverseMap();
+            CreateMap<UserAppDTO, UserApp>().ReverseMap();
+
+        }
+
+    }
+}
---
+++ AKAR_JWT_PROJECT/AKAR.AuthService.Services/Services/GenericService.cs
@@ -0,0 +1,51 @@
+﻿using AKAR.AuthServer.Core.Services;
+using AKAR.AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKAR.AuthService.Services.Services
+{
+    public class GenericService<T, TDto> : IServiceGeneric<T, TDto> where T : class where TDto : class
+    {
+        private readonly IUnitOfWork _unitOfWork;
+ 
+            
+            
+            
+        public Task<Response<TDto>> GetByIdAsync(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<IEnumerable<TDto>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<IEnumerable<TDto>>> Where(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<TDto>> AddAsync(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDTO>> DeleteAsync(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDTO>> UpdateAsync(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
